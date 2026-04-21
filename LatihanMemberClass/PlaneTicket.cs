ef4003e61c4f6d8a90f605a2a5689041bc3cdbcd
muhammad winner <muhammadwinner25@gmail.com>
2026-04-21 22:08:46 +0700

    Add files via upload

---
+++ LatihanMemberClass/PlaneTicket.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    internal class PlaneTicket
+    {
+        // Mendeklarasikan backing fields (variabel private)
+        private string _origin;
+        private string _destination;
+        private int _cost;
+        private string _currency;
+
+        public string Origin
+        {
+            get // get accessor
+            {
+                return _origin;
+            }
+            set // set accessor
+            {
+                _origin = value;
+            }
+        }
+
+        public string Destination
+        {
+            get // get accessor
+            {
+                return _destination;
+            }
+            set // set accessor
+            {
+                _destination = value;
+            }
+        }
+
+        public int Cost
+        {
+            get // get accessor
+            {
+                return _cost;
+            }
+            set // set accessor
+            {
+                _cost = value;
+            }
+        }
+
+        public string Currency
+        {
+            get // get accessor
+            {
+                return _currency;
+            }
+            set // set accessor
+            {
+                _currency = value;
+            }
+        }
+    }
+}
---
+++ LatihanMemberClass/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PlaneTicket ticket1 = new PlaneTicket();
+            PlaneTicket ticket2 = new PlaneTicket();
+
+            ticket1.Origin = "JOGJA";
+            ticket1.Destination = "PKU";
+            ticket1.Cost = 100;
+            ticket1.Currency = "USD";
+
+            ticket2.Origin = "JKT";
+            ticket2.Destination = "PKU";
+            ticket2.Cost = 750000;
+            ticket2.Currency = "IDR";
+
+            Console.WriteLine("Origin : {0}", ticket1.Origin);
+            Console.WriteLine("Destination : {0}", ticket1.Destination);
+            Console.WriteLine("Cost : {0}", ticket1.Cost);
+            Console.WriteLine("Convert to Rupiah : {0}", ticket1.Cost * 15000);
+
+            Console.WriteLine("Origin : {0}", ticket2.Origin);
+            Console.WriteLine("Destination : {0}", ticket2.Destination);
+            Console.WriteLine("Cost : {0}", ticket2.Cost);
+            Console.WriteLine("Convert to USD : {0}", ticket2.Cost / 15000);
+
+
+        }
+
+    }
+}
